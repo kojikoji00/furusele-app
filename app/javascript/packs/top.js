e71f39e39b28fbc081f6651251d3b65490801b61
kojikoji00 <y_koji_0807@yahoo.co,jp>
2021-01-03 23:47:33 +0900

    javascript

--- app/javascript/packs/top.js
+++ app/javascript/packs/top.js
@@ -1,1 +1,20 @@
 // import $ from 'jquery'
+// import axios from 'modules/axios'
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('.form-group').on('change',() => {
+    $('.deduction').removeClass('hidden')
+  })
+
+  $('.category_first').on('change',() => {
+    $('.select_category_title').append(
+      `<div class='select_category_title_first'><p>${'.category_first'}</p></div>`
+    )
+  })
+
+  $('.category_second').on('change',() => {
+    $('.select_category_title').append(
+      `<div class='select_category_title_first'><p>${'.category_first'}</p></div>`
+    )
+  })
+})
