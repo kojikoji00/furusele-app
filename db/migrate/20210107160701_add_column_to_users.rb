f2d6807149a14f82b74b8a3e8308f992689a237c
kojikoji00 <y_koji_0807@yahoo.co,jp>
2021-01-11 11:04:53 +0900

    income・familyをUserテーブルに追加

--- db/migrate/20210107160701_add_column_to_users.rb
+++ db/migrate/20210107160701_add_column_to_users.rb
@@ -1,5 +1,7 @@
 class AddColumnToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :nickname, :string, null: false, default: "sample"
+    add_column :users, :income, default: "", :string, null: false
+    add_column :users, :family, default: "", :string, null: false
   end
 end
