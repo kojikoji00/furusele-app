35fe1f3eea0cf6c7ad2bd15f6489d93df701b785
kojikoji00 <y_koji_0807@yahoo.co,jp>
2021-01-23 00:12:30 +0900

    slickの導入によりスライダー機能をつけた

--- app/javascript/packs/top.js
+++ app/javascript/packs/top.js
@@ -27,3 +27,11 @@ document.addEventListener('DOMContentLoaded', () => {
     )
   })
 })
+
+// $('.slider').slick();
+$('.slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
