c55d8bdf371092d49202077726b370db197ac1f9
kojikoji00 <y_koji_0807@yahoo.co,jp>
2021-01-28 23:07:19 +0900

    familyテーブル・incomeテーブルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'top#index'
   resource :select, only: %i[show index new create edit destroy]
+  resource :deduction_list, only: %i[show]
 end
