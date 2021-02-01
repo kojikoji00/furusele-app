4893d91bf5d04f52b5c43c6e480a9f9c009d759d
kojikoji00 <y_koji_0807@yahoo.co,jp>
2021-02-02 00:31:32 +0900

    必要となる項目をスクレイピングで呼び出せるようにした

--- app/models/scrapes.rb
+++ app/models/scrapes.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'json'
 
-url  = 'https://www.satofull.jp/'
+url  = 'https://www.satofull.jp/products/list.php?cnt=60&p=1'
 
 charset = nil
 html = open(url) do |f|
@@ -13,14 +13,23 @@ end
 
 doc = Nokogiri::HTML.parse(html, nil, charset)
 
-nodes = doc.xpath('//section[@class="Section"]')
-node = nodes.xpath('//div[@class="SideBox__body"]')
+# nodes = doc.xpath('//section[@class="Section"]')
+# nodes = doc.xpath('//ul[@class="ItemList"]')
+nodes = doc.xpath('//ul[@class="ItemList"]')
+# node = nodes.xpath('//div[@class="ItemList__body"]')
+node = nodes.css('li')
 
 node.each do |category|
-  category_title = category.css('a').text
-  category_link = category.css('a[href]')
+  category_link = category.css('a').attribute('href')
+  category_title = category.xpath('//p[@class="ItemList__name"]').inner_text
+  category_city = category.xpath('//p[@class="ItemList__city"]').inner_text
+  category_price = category.xpath('//p[@class="ItemList__price"]').inner_text
+  # category_review = category.xpath('//p[@class="ItemList__review"]')
   puts category_title
+  puts category_city
+  puts category_price
   puts category_link
+  # puts category_review
 end
 
 
