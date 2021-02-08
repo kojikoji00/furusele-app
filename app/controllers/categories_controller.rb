74c700937816aee376e73422e8637320ad53d04a
kojikoji00 <y_koji_0807@yahoo.co,jp>
2021-02-09 08:41:52 +0900

    Categoryの中でページ遷移

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,24 +1,78 @@
 class CategoriesController < ApplicationController
 
+  # def index
+  #   @categories = Category.all
+  # end
+
   def new
     @category = Category.new
     @categories = Category.all
-
+    # @category_detail = CategoryDetails.new
+    @category_details = CategoryDetail.all
   end
+
   def create
     @category = Category.new(category_params)
-    if @category.save
-    else
-      rebder :new
-    end
+    redirect_to category_path(@category)
   end
+    # @category_detail = Category_detail.build(item_list)
+    # category_detail = CategoryDetail.new(category_detail_params)
 
-  def index
-    @categories = Category.all
+  def show
+    @category = Category.find(params[:id])
+    # @category_details = CategoryDetail.where(category_id: @category.id)
+    income = current_user.income_id
+    family = current_user.family_id
+    @deduction = DeductionList.find_by(income_id: income, family_id: family)
   end
 
   private
   def category_params
-    params.require(:category.permit(:name))
+    params.require(:category).permit(:id)
+  end
+  def category_detail_params
+    params.require(:category_detail).permit(:category_id)
+  end
+
+  def item_list
+    require 'nokogiri'
+    require 'open-uri'
+    require 'json'
+    
+    url = 'https://www.satofull.jp/products/list.php?cat=' + Category.find(:id).satofull_id
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
+  
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    nodes = doc.xpath('//ul[@class="ItemList"]')
+  
+    array = []
+    lists = []
+  
+    nodes.css('a').first(5).each do |node|
+      link = 'https://www.satofull.jp' + node[:href]
+      name = node.css('.ItemList__name').text
+      city = node.css('.ItemList__city').text
+      price = node.css('.ItemList__price>span').text.delete(',').to_i
+      description = node.css('.ItemList__description').text
+      picture_image_path = 'https://www.satofull.jp' + node.css('.ItemList__picture>img').attribute('src')
+      review_image_path = 'https://www.satofull.jp' + node.css('.ItemList__review>img').attribute('src')
+      array << [link, name, city, price,description, picture_image_path, review_image_path]
+    end
+    array.each do |t|
+      # item = HistoryDetail.new
+      # item.history_id = @history.id
+      item.link = t[0]
+      item.name = t[1]
+      item.city = t[2]
+      item.price = t[3]
+      item.description = t[4]
+      item.picture_image_path = t[5]
+      item.review_image_path = t[6]
+      # item.save
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'top#index'
   resource :deduction_list, only: %i[show]
-  resources :categories
+  resources :categories, only: %i[new create show] do
+    resources :category_details, only: %i[new create show]
+  end
   resources :histories, only: %i[index new show create] do
     resources :history_details, only: %i[index]
   end
